2455a8a4ac11aaee018365f0e6cea28eb3a47a05
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-08-30 06:30:28 +0000

    update code

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     ssl_context.load_cert_chain(
         '/certs/image-policy.crt',
-        '/certs/image-policy-key.pem'
+        '/certs/image-policy.key'
     )
 
     APP.run(host="0.0.0.0", port=10443, ssl_context=ssl_context)
